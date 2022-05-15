298ed6d34c062ffecd109e0484cb2006d9c4f3cf
leandrocm86 <30850489+leandrocm86@users.noreply.github.com>
2022-05-15 08:00:28 -0300

    Pip
    Arquivo de configuração para pip

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='PyUtils',
+    version='0.1',
+    author='Leandro Medeiros',
+    author_email='leandrocm86@gmail.com',
+    description='Classes utilitárias para Python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/leandrocm86/PyUtils',
+    project_urls = {
+        "Bug Tracker": "https://github.com/leandrocm86/PyUtils/issues"
+    },
+    license='MIT',
+    packages=['PyUtils'],
+    install_requires=[],
+)
