698c383464fc5de555cbd763398d7b8ceff14193
leandrocm86 <leandrocm86@gmail.com>
2023-01-03 21:14:46 -0300

    v0.25 - Added 'maybe' function for safe evaluations

--- pyutils/maybe.py
+++ pyutils/maybe.py
@@ -1,4 +1,6 @@
 def maybe(function):
+    """ Evaluates the given function in a safe manner, supressing any possible exception.\n"""
+    """ If no exception occurs, the function's output is returned. Otherwise, None is returned."""
     try:
         return function()
     except:
